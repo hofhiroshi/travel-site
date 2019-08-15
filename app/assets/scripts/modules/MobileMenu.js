5390a2caf7418697e43631f1abd11d0f4f82602f
Brad <boston164@gmail.com>
2019-08-15 05:45:32 -0400

    files.associations is added to settings.json

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -10,6 +10,7 @@ class MobileMenu {
 
     // +++ another way +++ 
     constructor(){
+        this.siteHeader = $(".site-header");
         this.menuIcon = $(".site-header__menu-icon");
         this.menuContent = $(".site-header__menu-content");
         this.events();
@@ -21,6 +22,8 @@ class MobileMenu {
 
     toggleTheMenu() {
         this.menuContent.toggleClass("site-header__menu-content--is-visible");
+        this.siteHeader.toggleClass("site-header--is-expanded");
+        this.menuIcon.toggleClass("site-header__menu-icon--close-x");
     }
 
 }
