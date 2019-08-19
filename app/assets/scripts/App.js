4dbcefa40039c86aac71f6bade61ecc716e1bec7
Brad <boston164@gmail.com>
2019-08-19 11:57:58 -0400

    Lesson 46 completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 // import variableName from path to the module 
 // variableName can be anything
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
 
